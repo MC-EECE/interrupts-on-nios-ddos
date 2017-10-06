d8ed5e175cabb200cfde335d42224b0a061108cb
jstefanou <31664733+jstefanou@users.noreply.github.com>
2017-10-06 16:25:18 -0400

    Final Product Lab 2
    Stopwatch using intrupts

--- HexDisplay.c
+++ HexDisplay.c
@@ -4,13 +4,23 @@
  *  Created on: Apr 21, 2015
  *      Author: brent.horine
  */
+#include "io.h"
+#include "system.h"
+#include "alt_types.h"
+
 #include "seven_seg_hex.h"
 #include "seven_seg_bcd.h"
 
-extern volatile alt_u8 leading_zeros;
-extern volatile alt_u8 display_as_bcd;
+//extern volatile alt_u8 leading_zeros;
+static volatile alt_u8 display_as_bcd = 1;
+
+void toggleBCDHEX()
+{
+	display_as_bcd = !display_as_bcd;
+}
 
 void HexDisplay(alt_u32 * base, alt_u16 num) {
+	alt_u8 leading_zeros = 0x1&IORD(SLIDER_SWITCHES_BASE, 0);
 	if (display_as_bcd) {
 		seven_seg_bcd(base, num, leading_zeros);
 	} else {
--- InterruptDemo.c
+++ InterruptDemo.c
@@ -2,6 +2,9 @@
 #include "io.h"
 #include "system.h"
 #include "alt_types.h"
+#include "sys/alt_irq.h"
+#include "timerISR.h"
+#include "buttonISR.h"
 
 /*******************************************************************************
  * int main()                                                                  *
@@ -11,13 +14,24 @@
 int main(void)
 { 
     /* Setup */
+	alt_u32 * pCount;
+	static alt_u32 count = 0;
+	pCount = &count;
 
     /* Initialize Variables */
 
     /* Register ISRs */
-
+	alt_irq_register(PUSHBUTTONS_IRQ, (void*)pCount, buttonISR);
+	alt_irq_register(INTERVAL_TIMER_IRQ, (void*)pCount, timerISR);
     /* Initialize Timer */
+	// Setup start value
+	IOWR(INTERVAL_TIMER_BASE, 2, 0xFFF&INTERVAL_TIMER_FREQ);
+	IOWR(INTERVAL_TIMER_BASE, 3, 0xFFF&(INTERVAL_TIMER_FREQ >> 16));
+	//Start the timer
+	IOWR(INTERVAL_TIMER_BASE, 1, 0x7); // Stop=0, Start=1, Cont=1, ITO=1 ==> 0111b = 0x7
 
+	/* initialize Pushbuttons*/
+	IOWR(PUSHBUTTONS_BASE, 2, 0xE); // 1110b= 0xE (0xF would work too, but you will never see
     /* Loop while processing interrupts */
     while( 1 ) 
     {
